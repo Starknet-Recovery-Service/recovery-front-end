40f194ca3f2b4af8acee56736ed69c82e67eaa52
remi-gai <56744348+remi-gai@users.noreply.github.com>
2022-08-13 10:37:13 +0200

    added the main components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./Nav";
+import RecoveryDeployment from "./RecoveryDeployment";
+import RecoveryExecution from "./RecoveryExecution";
+import Allowance from "./Allowance";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <RecoveryDeployment />
+      <RecoveryExecution />
+      <Allowance />
     </div>
   );
 }
