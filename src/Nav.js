d3ca359a1e6fbfbb6baf9c0c58a8c1b691393cd6
remi-gai <56744348+remi-gai@users.noreply.github.com>
2022-08-15 15:44:29 +0200

    fixed size

--- src/Nav.js
+++ src/Nav.js
@@ -131,7 +131,9 @@ export default function Simple({ setPage }) {
           <Box pb={4} display={{ md: "none" }}>
             <Stack as={"nav"} spacing={4}>
               {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+                <NavLink setPage={setPage} key={link}>
+                  {link}
+                </NavLink>
               ))}
             </Stack>
           </Box>
